b2b5180698546597b7b1186db93200e53242c649
Debjit Majumder <majumderdebjit@gmail.com>
2021-06-30 00:07:18 +0530

    chat-app-client pushed

---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,77 @@
+import React, { useState, useEffect } from "react";
+import queryString from "query-string";
+import io from "socket.io-client";
+import InfoBar from "../InfoBar/InfoBar";
+import Input from "../Input/Input";
+import Messages from "../Messages/Messages";
+import TextContainer from "../TextContainer/TextContainer";
+import "./Chat.css";
+
+let socket;
+function Chat({ location, history }) {
+  const [name, setName] = useState("");
+  const [room, setRoom] = useState("");
+  const [message, setMessage] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [users, setUsers] = useState([]);
+
+  const ENDPOINT = "http://localhost:5000";
+
+  useEffect(() => {
+    const { name, room } = queryString.parse(location.search);
+    console.log(name, room, history);
+    socket = io(ENDPOINT);
+
+    setName(name);
+    setRoom(room);
+
+    socket.emit("join", { name, room }, (error) => {
+      console.log(error);
+      if (error) {
+        alert(error);
+        return history.push("/");
+      }
+    });
+
+    return () => {
+      socket.disconnect();
+      socket.off();
+    };
+  }, [ENDPOINT, location.search, history]);
+
+  useEffect(() => {
+    socket.on("message", (message) => {
+      setMessages([...messages, message]);
+    });
+
+    socket.on("roomData", ({ users }) => {
+      setUsers(users);
+    });
+  }, [messages]);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    if (message) {
+      socket.emit("sendMessage", message, () => {
+        setMessage("");
+      });
+    }
+  };
+
+  return (
+    <div className="outerContainer">
+      <div className="container">
+        <InfoBar room={room} />
+        <Messages messages={messages} name={name} />
+        <Input
+          message={message}
+          setMessage={setMessage}
+          sendMessage={sendMessage}
+        />
+      </div>
+      <TextContainer users={users} />
+    </div>
+  );
+}
+
+export default Chat;
